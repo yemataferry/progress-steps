5d5af6c0feb214a281e0a5f07f44650f0ae96fb3
Yemata Ferry <yemataferry@gmail.com>
2024-01-06 06:16:05 +0100

    linter checks

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -35,7 +35,7 @@ function update () {
 
   const actives = document.querySelectorAll('.active')
 
-  progress.style.width = (actives.length -1) / (circles.length -1) * 100 + '%'
+  progress.style.width = (actives.length - 1) / (circles.length - 1) * 100 + '%'
   if (currentActive === 1) {
     prev.disabled = true
   } else if (currentActive === circles.length) {
